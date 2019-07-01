d5752b395b1277769504caf080f1b5da31acd606
Irina K <irtiola@gmail.com>
2019-07-01 07:20:49 -0700

    js with html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,41 @@
-var wins
-var losses
-var guesses = 10;
+var num_wins = 0;
+var num_losses = 0;
+var num_guesses = 9;
+var so_far_guesses = [];
+var random_letter;
 
-for (i=0; i<10;i++){
 
+function ResetStart(){
+    num_guesses = 9;
+    so_far_guesses = [];
+    letter = Math.floor(Math.random()*26);
+    random_letter = String.fromCharCode(letter +97); //ascii table a letter is 97
+    UpdateScreen();
+
+}
+
+function UpdateScreen() {
+    document.getElementById('num_wins').textContent = num_wins;
+    document.getElementById('num_losses').textContent = num_losses;
+    document.getElementById('num_guesses').textContent = num_guesses;
+    document.getElementById('so_far_guesses').textContent = so_far_guesses.join(", ");
+    
+}
+
+function Play(){
+    user_guess = String.fromCharCode(event.keyCode);
+    so_far_guesses.push(user_guess);
+    if (user_guess == random_letter){
+        num_wins++;
+        ResetStart();
+    }
+    else {
+        num_guesses--;
+        if(num_guesses < 1){
+            num_losses++;
+            ResetStart();
+        }
+        
+    }
+    UpdateScreen();
 }
