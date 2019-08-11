b22a6a433d62bf09986d41068a4fcd95ec2e4bb5
Irina K <irtiola@gmail.com>
2019-08-10 18:43:57 -0700

    indentations

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,11 +5,11 @@ var so_far_guesses = [];
 var random_letter;
 
 
-function ResetStart(){
+function ResetStart() {
     num_guesses = 9;
     so_far_guesses = [];
-    letter = Math.floor(Math.random()*26);
-    random_letter = String.fromCharCode(letter +97); //ascii table a letter is 97
+    letter = Math.floor(Math.random() * 26);
+    random_letter = String.fromCharCode(letter + 97); //ascii table a letter is 97
     UpdateScreen();
 
 }
@@ -19,23 +19,23 @@ function UpdateScreen() {
     document.getElementById('num_losses').textContent = num_losses;
     document.getElementById('num_guesses').textContent = num_guesses;
     document.getElementById('so_far_guesses').textContent = so_far_guesses.join(", ");
-    
+
 }
 
-function Play(){
+function Play() {
     user_guess = String.fromCharCode(event.keyCode);
     so_far_guesses.push(user_guess);
-    if (user_guess == random_letter){
+    if (user_guess == random_letter) {
         num_wins++;
         ResetStart();
     }
     else {
         num_guesses--;
-        if(num_guesses < 1){
+        if (num_guesses < 1) {
             num_losses++;
             ResetStart();
         }
-        
+
     }
     UpdateScreen();
 }
